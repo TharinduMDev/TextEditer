0f48d3033080417ce13e407ef6946d6aa1004f28
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-09-25 01:51:42 +0530

    Update Open File

---
+++ README.md
@@ -0,0 +1,7 @@
+# Text Editor
+
+## Version 0.1.0
+
+### License
+Copyright and copy: 2023 Tharidu Madushan <br>
+This project is license under this [MIT LICENSE](License.txt)
--- src/main/java/lk/ijse/dep11/MainFormController.java
+++ src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep11;
 
+import com.sun.glass.ui.CommonDialogs;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -20,7 +21,11 @@ import org.jsoup.select.Elements;
 
 import javax.swing.text.Document;
 import java.io.*;
+import java.lang.reflect.AnnotatedArrayType;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainFormController {
     public MenuItem mnExit;
@@ -82,47 +87,50 @@ public class MainFormController {
     }
 
     public void mnSaveAsOnAction(ActionEvent actionEvent) throws  IOException{
-//        System.out.println(txtTextEditor.);
+//
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Save the file");
         fileChooser.setInitialFileName("Untitled Document.txt");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Text Files (*.txt, *.html)", "*.txt", "*.html"));
         File file = fileChooser.showSaveDialog(txtTextEditor.getScene().getWindow());
+        currentFile = file;
         if(file == null)
             return;
-
-        FileOutputStream os = new FileOutputStream(file);
-        BufferedOutputStream bos = new BufferedOutputStream(os);
-        //Todo
-        Document doc = (Document) Jsoup.parse(String.format(txtTextEditor.getHtmlText()));
-       // Elements elements = doc.getAllElements();
-//
-//        StringBuilder plainText = new StringBuilder();
-//
-//        // Iterate through the elements and extract their text
-//        for (Element element : elements) {
-//            String text = element.ownText(); // Get the text of this element (excluding child elements)
-//            if (!text.isEmpty()) {
-//                plainText.append(text).append("\n");
-//            }
-//        }
-
-//        String htmlContent = ;
-//
-//        bos.write(extractTextFromHTML(txtTextEditor.getHtmlText()));
-        bos.close();
-
-
+        saveText(currentFile);
     }
 
     public void mnSaveOnAction(ActionEvent actionEvent) throws IOException {
         if(currentFile == null){
             mnSaveAsOnAction(actionEvent);
         }else {
-            FileOutputStream os = new FileOutputStream(currentFile);
+            saveText(currentFile);
+        }
+    }
+
+    public void saveText(File file) throws IOException {
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(file);
             BufferedOutputStream bos = new BufferedOutputStream(os);
-            //Todo
-            bos.close();
+            try {
+                String htmlText1 = "<html dir=\"ltr\"><head></head><body contenteditable=\"true\"><p><span style=\"font-family: &quot;&quot;;\">";
+                String htmlText2 = "</span></p></body></html>";
+                Pattern pat = Pattern.compile("</span></p><p><span style=\"font-family: &quot;&quot;;\">");
+                String text = txtTextEditor.getHtmlText().substring(htmlText1.length(),txtTextEditor.getHtmlText().length()-htmlText2.length());
+                Matcher matcher = pat.matcher(text);
+                while (matcher.find()) {
+
+                    text = text.substring(0,matcher.start())+ text.substring(matcher.end(),text.length());
+                }
+                System.out.println(text);
+                bos.write(text.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                bos.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
@@ -135,13 +143,17 @@ public class MainFormController {
             return;
         }
         currentFile = file;
-        FileInputStream fis = new FileInputStream(file);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        byte[] buffer = new byte[1024];
-        int read = -1;
-        while ((read = bis.read(buffer))!= -1){
-            txtTextEditor.setHtmlText(new String(buffer,0,read));
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String text = "";
+        String read1 = "";
+        while ((read1 = bufferedReader.readLine()) != null){
+//            System.out.println(read1);
+            text += read1+"</span></p><p><span style=\"font-family: &quot;&quot;;\">";
+
         }
-        bis.close();
+        System.out.println(text);
+        txtTextEditor.setHtmlText(text);
+        bufferedReader.close();
     }
 }
